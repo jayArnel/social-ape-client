662ff3184e565e49b2b394941fbc3360efb9ea6f
Jay Arnel Bilocura <jay.arnel@gmail.com>
2020-07-10 11:26:17 +0800

    Created profile section

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 
 // components
 import Scream from '../components/Scream';
+import Profile from "../components/Profile";
 
 class home extends Component {
   state = {
@@ -31,7 +32,7 @@ class home extends Component {
           {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
-          <p>Profile</p>
+          <Profile/>
         </Grid>
       </Grid>
     );
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,4 +1,10 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types";
+import {
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  LOADING_USER,
+} from "../types";
 import axios from "axios";
 
 export const loginUser = (userData, history) => (dispatch) => {
@@ -43,6 +49,7 @@ export const logoutUser = () => (dispatch) => {
 };
 
 export const getUserData = () => (dispatch) => {
+  dispatch({ type: LOADING_USER });
   axios
     .get("/user")
     .then((res) => {
